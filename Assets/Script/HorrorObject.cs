148e3554c3573a78a7a98ed9dddb4f65e02b47f0
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-07-07 16:19:09 +0800

    Initial commit

---
+++ Assets/Script/HorrorObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorrorObject : MonoBehaviour , ITriggerEvent
+{
+
+
+    public virtual void TriggerEvent()
+    {
+
+        print("去死吧 by Reim");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player")) TriggerEvent();
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(1,1000)]
+    public float speed = 10;
+    public float turn = 5;
+    float time = 120;
+
+    public delegate void D_PlayerEvent() ;
+    public event D_PlayerEvent onDead;
+    public event D_PlayerEvent onItem;
+    public event D_PlayerEvent onFinal;
+
+    private Rigidbody rig;
+    Transform cam;
+
+
+
+
+
+    void Move() {
+        float v = Input.GetAxis("Vertical")/10;
+        float h = Input.GetAxis("Horizontal")/10;
+        rig.AddForce(cam.forward * v * speed + cam.right * h * speed);
+
+        float x = Input.GetAxis("Mouse X");
+        cam.Rotate(0, x * turn, 0);    
+    }
+
+
+
+
+
+
+
+
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        cam = transform.GetChild(0);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+}
